f1eeab93b7056838f4722158128bb00851ddeca2
dhruva1956 <dhiren2910dommeti@gmail.com>
2024-11-19 20:39:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(app, host='0.0.0.0', debug=True, port=5000)
+    #app.run(debug=True)
